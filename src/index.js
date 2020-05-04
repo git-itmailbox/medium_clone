5d562fcbf2b682f1c4b4455a4ae18acf0b592df4
Yurii.Shportko <yure.new.ys@gmail.com>
2020-05-04 22:55:52 +0300

    install react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
 
+import Routes from 'routes'
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to hooks</h3>
+      <Router>
+        <Routes />
+      </Router>
+
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
