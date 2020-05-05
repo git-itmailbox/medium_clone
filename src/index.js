3e6f8bf3ddb3269fad8849ca55d86e9f99bf2584
Yurii.Shportko <yure.new.ys@gmail.com>
2020-05-05 09:51:30 +0300

    added navbar

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
+import TopBar from 'components/topBar'
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to hooks</h3>
       <Router>
+        <TopBar />
         <Routes />
       </Router>
 
